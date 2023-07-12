dcb113b82f5b13fb7db9a8b61d8db7e66bffa348
jrpompio <junior.ruiz@ucr.ac.cr>
2023-07-12 03:27:41 -0600

    exe

--- prueba_proyecto1.c
+++ prueba_proyecto1.c
@@ -11,7 +11,7 @@ char* factorizarPrimos(long long int numero) {
     while (numero > 1) {
         if (numero % factor == 0) {
             char temp[100];
-            sprintf(temp, "*%d", factor);
+            sprintf(temp, "#%d", factor);
             strcat(factores, temp);
             numero /= factor;
         } else {
@@ -22,6 +22,73 @@ char* factorizarPrimos(long long int numero) {
     free(factores);
 }
 
+/*******************************************************************************
+Insertando código a probar
+*******************************************************************************/
+
+// Esta función toma como entrada una cadena de caracteres y cuenta el número de veces que cada dígito aparece consecutivamente en la cadena.
+void contarRepeticiones(const char* cadena) {
+    // Primero, calculamos la longitud de la cadena usando la función strlen.
+    int longitud = strlen(cadena);
+    int i, j, k;
+
+    // Luego, usamos un bucle for para recorrer cada carácter de la cadena.
+    for (i = 0; i < longitud; i++) {
+        // Si el carácter actual es '#' y es el primer carácter de la cadena o el carácter anterior no es '#', entonces entramos en este bloque de código.
+        if (cadena[i] == '#' && (i == 0 || cadena[i-2] != cadena[i])) {
+            // Calculamos el valor numérico del dígito que sigue al carácter '#' restando el valor ASCII del carácter '0' al valor ASCII del dígito.
+            int num = cadena[i + 1] - '0';
+            // Inicializamos el contador de repeticiones en 1.
+            int repeticiones = 1;
+
+            // Usamos otro bucle for para recorrer los siguientes caracteres de la cadena, avanzando de dos en dos para saltar los caracteres '#'.
+            for (j = i + 2; j < longitud; j+=2) {
+                // Si el carácter actual es '#', entonces entramos en este bloque de código.
+                if (cadena[j] == '#') {
+                    // Calculamos el valor numérico del dígito que sigue al carácter '#' restando el valor ASCII del carácter '0' al valor ASCII del dígito.
+                    int siguienteNum = cadena[j + 1] - '0';
+                    // Si el siguiente dígito es igual al dígito que estamos contando, entonces incrementamos el contador de repeticiones en 1.
+                    if (siguienteNum == num) {
+                        repeticiones++;
+                    } else {
+                        // Si el siguiente dígito no es igual al dígito que estamos contando, entonces salimos del bucle for interno.
+                        break;
+                    }
+                }
+            }
+
+            // Imprimimos el dígito y su número de repeticiones en el formato "(dígito, repeticiones)".
+            printf("(%d, %d) ", num, repeticiones);
+            // Actualizamos el índice del bucle for externo para saltar los caracteres que ya hemos contado.
+            i = j-2;
+        }
+    }
+
+    // Usamos otro bucle for para encontrar el índice del primer carácter que no sea igual al primer carácter de la cadena o cuyo dígito no sea igual al primer dígito de la cadena.
+    for (i = 2; i < longitud; i += 2) {
+        if (cadena[i] != cadena[0] || cadena[i + 1] != cadena[1]) {
+            break;
+        }
+    }
+
+    // Creamos una nueva cadena llamada nuevaCadena que tiene la misma longitud que la cadena original menos el número de elementos que estamos eliminando.
+    char* nuevaCadena = malloc(longitud - i + 1);
+    // Copiamos todos los elementos de la cadena original a partir del índice encontrado en el bucle for anterior en la nueva cadena.
+    strcpy(nuevaCadena, cadena + i);
+
+    // Llamamos a la función contarRepeticiones recursivamente con la nueva cadena como argumento para contar las repeticiones de los dígitos restantes.
+    contarRepeticiones(nuevaCadena);
+
+    // Liberamos la memoria asignada para la nueva cadena usando la función free.
+    free(nuevaCadena);
+
+    printf("\n");
+}
+
+
+/*******************************************************************************
+Insertando código a probar
+*******************************************************************************/
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -33,7 +100,7 @@ int main(int argc, char *argv[]) {
 
     char* mensaje = factorizarPrimos(numero);
 
-    printf("\n%s\n\n", mensaje);
+    contarRepeticiones(mensaje);
 
     free(mensaje);
 
